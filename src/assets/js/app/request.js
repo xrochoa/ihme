78ffba558bcb95c467177cf6ec8fc60278a1042b
Xavier Reyes <xareyesochoa@gmail.com>
2016-11-21 10:41:55 -0800

    added custom made topojson (qgis, mapshaper)

--- src/assets/js/app/request.js
+++ src/assets/js/app/request.js
@@ -7,14 +7,31 @@ import { feature } from 'topojson-client';
 
 export function request(dispatcher) {
 
-    json('./assets/res/world-50m.json', function(error, world) {
+    /*----------  TOPOJSON MAP  ----------*/
+
+
+    json('./assets/res/world-custom.json', function(error, world) {
         if (error) throw error;
 
         //countries topojson
-        let countries = feature(world, world.objects.countries).features;
+        let countries = feature(world, world.objects.map).features;
 
         //emmiter when ready
-        dispatcher.call('JSON_LOADED', this, countries);
+        dispatcher.call('TOPOJSON_LOADED', this, countries);
+
+    });
+
+    /*----------  DATA  ----------*/
+
+    dispatcher.on('TOPOJSON_LOADED', function(countries) {
+
+        json('https://ihme-f3ac5.firebaseio.com/2013/38/3/overweight.json', function(error, countryData) {
+            if (error) throw error;
+
+            //emmiter when ready
+            dispatcher.call('JSON_LOADED', this, countries, countryData);
+
+        });
 
     });
 
