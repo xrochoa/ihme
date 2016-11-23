377c4c7c6d4bcb29c821fbbd051ba9a608f33a78
Xavier Reyes <xareyesochoa@gmail.com>
2016-11-22 16:03:35 -0800

    Update 2016-11-23T00:03:33.713Z

--- README.md
+++
@@ -1,6 +0,0 @@
-ihme
----
-## Run:
-'sudo npm install' - Install dependencies
-'gulp' - Build for development and serve at http://localhost:3000
-'gulp --production' - Build for production and serve at http://localhost:3000
--- src/assets/js/app/dispatcher.js
+++
@@ -1,7 +0,0 @@
-/*----------  LIBRARIES  ----------*/
-
-import { dispatch } from 'd3-dispatch';
-
-/*----------  DISPATCH EVENTS ----------*/
-
-export let dispatcher = dispatch('TOPOJSON_LOADED', 'LOAD_DATA', 'DATA_LOADED', 'COUNTRY_CHANGED');
